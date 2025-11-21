3ffb890f475b3ede4589ada75f4343e2bdd7930c
Pablo Valle <vpablo@MacBook-Air-de-Pablo.local>
2025-11-21 02:17:50 -0600

    Activando SSL para base de datos

--- servicio-visitas/db/index.js
+++ servicio-visitas/db/index.js
@@ -1,16 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Configuración para Nube (Railway) vs Local
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.RAILWAY_ENVIRONMENT ? { rejectUnauthorized: false } : false
+  // ESTA ES LA LÍNEA MÁGICA QUE TE FALTA:
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
-
-// Si no hay connection string, avisar del error en consola
-if (!process.env.DATABASE_URL) {
-    console.error("ERROR FATAL: No existe la variable DATABASE_URL. El sistema intentará conectarse a localhost y fallará.");
-}
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
